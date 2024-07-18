ff20f39609350906312a776028ee7d38bb3b677e
bajoelabrigo <bajoelabrigo@gmail.com>
2024-07-18 12:08:09 -0500

    midelware

--- middleware.ts
+++ middleware.ts
@@ -1,22 +1,13 @@
-import { authMiddleware } from "@clerk/nextjs";
- 
-export default authMiddleware({
-  publicRoutes: [
-    '/',
-    '/api/webhook',
-    'question/:id',
-    '/tags',
-    '/tags/:id',
-    '/profile/:id',
-    '/community',
-    '/jobs'
-  ],
-  ignoredRoutes: [
-    '/api/webhook', '/api/chatgpt'
-  ]
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+
+const isProtectedRoute = createRouteMatcher([
+  
+]);
+
+export default clerkMiddleware((auth, req) => {
+  if (isProtectedRoute(req)) auth().protect();
 });
- 
+
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
- 
